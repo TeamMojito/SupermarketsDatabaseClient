e58f42fa842760ad82e0b67801371c76a3b0b28b
d.georgiev.91 <d.georgiev.91@gmail.com>
2013-07-22 13:09:27 +0300

    Added Zip Extractor

---
+++ ExtractExcelReportsFromZip/ZipExtractor.cs
@@ -0,0 +1,40 @@
+﻿namespace ExtractExcelReportsFromZip
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipExtractor
+    {
+        private string extractDirectory;
+
+        public ZipExtractor(string extractionDirectory)
+        {
+            this.ExtractDirectory = extractionDirectory;
+        }
+
+        public string ExtractContentTo(string directoryToExtract)
+        {
+            if (!Directory.Exists(directoryToExtract))
+            {
+                Directory.CreateDirectory(directoryToExtract);
+            }
+
+            ZipFile.ExtractToDirectory(this.ExtractDirectory, directoryToExtract);
+
+            return directoryToExtract;
+        }
+
+        public string ExtractDirectory
+        {
+            get
+            {
+                return this.extractDirectory;
+            }
+            set
+            {
+                this.extractDirectory = value;
+            }
+        }
+    }
+}
---
+++ SupermarketsDatabaseClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExtractExcelReportsFromZip;
+
+namespace SupermarketsDatabaseClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipExtractor zipExtractor = new ZipExtractor(@"D:\Trash\Sample-Sales-Reports.zip");
+            zipExtractor.ExtractContentTo(@"D:\Trash\Reports");
+        }
+    }
+}
